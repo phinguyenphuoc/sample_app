f5d1dc89a6786359e7fe047d3092794a970c9a49
PhiNguyen <phinguyenphuoc@gmail.com>
2019-02-15 15:59:20 +0700

    chapter7: Sign up chapter3: Mostly static page
    chapter4: Rails-flavored Ruby
    
    chapter5: Filling in the layout
    
    chapter6: Modeling users
    
    chapter7: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  before_save{self.email = email.downcase}
-  validates :name, presence: true, length: {maximum: Settings.user.name_maxlength}
+  before_save{email.downcase!}
+  validates :name, presence: true,
+  length: {maximum: Settings.user.name_maxlength}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.user.email_maxlength},
-  format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Settings.user.pass_minlenght}
+  validates :email, presence: true,
+    length: {maximum: Settings.user.email_maxlength},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true,
+    length: {minimum: Settings.user.pass_minlenght}
   has_secure_password
 end
